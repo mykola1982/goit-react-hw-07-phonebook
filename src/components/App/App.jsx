38c7b6de538d88f529251b38d558b3dc71cb7d15
Mykola Zaikovskyi <mykola.zaikovskyi@gmail.com>
2022-12-04 12:07:55 +0200

    add localStorage

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -58,6 +58,23 @@ export class App extends Component {
     }));
   };
 
+  componentDidMount() {
+    try {
+      const contacts = localStorage.getItem('contacts');
+      const parsedContacts = JSON.parse(contacts);
+      if (parsedContacts) {
+        this.setState({ contacts: parsedContacts });
+      }
+    } catch (error) {
+      console.error(error.message);
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.contacts !== prevState.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
   render() {
     const { filter } = this.state;
     const visibleContacts = this.getVisibelContats();
